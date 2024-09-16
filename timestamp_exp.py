17592409bfc78451af85431ed154a3213694b0a1
Roja-kumar-25 <roja@aggregateintelligence.in>
2024-09-16 12:28:39 +0530

    updated

--- timestamp_exp.py
+++ timestamp_exp.py
@@ -9,6 +9,18 @@ data=[["1","2020-02-01"],["2","2019-03-01"],["3","2021-03-01"]]
 df=spark.createDataFrame(data,["id","input"])
 df.show()
 
+#to check the current date
 df.select(current_date().alias("current_date")).show()
 
+#to check the date format
 df.select(col("input"),date_format(col("input"),"MM-dd-yyyy").alias("date-format")).show()
+
+#to convert the input column to date column
+df.select(col("input"),to_date(col("input"),"yyyy-MM-dd").alias("to_date")).show()
+
+df.select(col("input")).withColumn("date-difference",date_diff(current_date(),col("input"))).show()
+
+# df.select(col("input")).withColumn("mnth-between",months_between(current_date(),col("input"))).orderBy("mnth_between").show()
+
+df.select(col("input"),current_timestamp().alias("current-time")).show()
+
